fd42d8feacfd1af801b0a5674be632b06f145147
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-31 20:36:48 +0500

    test2

--- part2/fury/main.py
+++ part2/fury/main.py
@@ -35,9 +35,14 @@ class Unit:
             raise UnitDied(f'Трагически погиб в неравном бою {self.name}')
         return True
 
+
 class FuriousHero(Unit):
-    pass
-    #TODO напишите логику класса здесь
+
+    def _get_damage(self, damage):
+        if damage > self.defence:
+            self.power += 1
+        super()._get_damage(damage)
+
 
 # Логика кода ниже такая же, как и в предыдущем задании.
 # Если вдруг интересно, то можно
